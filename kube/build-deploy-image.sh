273e347962bd309400f4d2dfe1ca224cfad3974d
kidus.sw@ibm.com <kidus.sw@ibm.com@10.0.0.155>
2019-08-29 12:27:36 -0400

    ddf

--- kube/build-deploy-image.sh
+++ kube/build-deploy-image.sh
@@ -3,8 +3,8 @@
 # 2. Upload the image to our repository if registry env variables are defined
 set -e
 
-DOCKER_IMAGE_NAME=kabanero-landing 
-NAMESPACE=/r/kidus60/kabanero
+DOCKER_IMAGE_NAME=kabanero-landing
+NAMESPACE=kidus60
 
 CUR_DIR="$(cd $(dirname $0) && pwd)"
 
@@ -18,7 +18,11 @@ docker build -t "$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG" -f "$CUR_DIR"/Dockerfile 
 
 # publish to a registry
 if [ -n "$DOCKER_REGISTRY" ] && [ -n "$DOCKER_REGISTRY_USER" ] && [ -n "$DOCKER_REGISTRY_PASSWORD" ]; then
-    docker login "--username=${DOCKER_REGISTRY_USER}" "--password=${DOCKER_REGISTRY_PASSWORD}" "${DOCKER_REGISTRY}"
+    docker tag "$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG" "$DOCKER_REGISTRY/$NAMESPACE/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG"
+    echo "Connecting to $DOCKER_REGISTRY Docker registry as $DOCKER_REGISTRY_USER"
 
-docker push $DOCKER_REGISTRY/$IMAGE_NAME
+    docker login "--username=$DOCKER_REGISTRY_USER" "--password=$DOCKER_REGISTRY_PASSWORD" "$DOCKER_REGISTRY"
+
+    echo "Pushing $DOCKER_REGISTRY/$NAMESPACE/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG image to Docker registry"
+    docker push "$DOCKER_REGISTRY/$NAMESPACE/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG"
 fi
