40fda8646906c6482bbc739dd4a4d78911699592
kidus.sw@ibm.com <kidus.sw@ibm.com@10.0.0.155>
2019-12-18 13:29:04 -0500

    client api

--- src/main/java/io/kubernetes/KabaneroClient.java
+++ src/main/java/io/kubernetes/KabaneroClient.java
@@ -30,6 +30,7 @@ import io.kubernetes.client.Configuration;
 import io.kubernetes.client.apis.CustomObjectsApi;
 import io.kubernetes.client.util.ClientBuilder;
 import io.kubernetes.client.util.KubeConfig;
+import io.kubernetes.client.util.Watch;
 
 import java.io.File;
 import java.io.FileReader;
@@ -42,6 +43,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -49,10 +51,13 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
+import com.google.gson.reflect.TypeToken;
 import com.squareup.okhttp.ConnectionSpec;
 
 public class KabaneroClient {
     private final static Logger LOGGER = Logger.getLogger(KabaneroClient.class.getName());
+
+    private static int resourceVersion = 0;
 
     // routes from kabanero namespace
     private static String getLabeledRoute(String Label, Map<String, Route> routes) {
@@ -95,6 +100,9 @@ public class KabaneroClient {
         ConnectionSpec spec = new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS).allEnabledCipherSuites().build();
         client.getHttpClient().setConnectionSpecs(Collections.singletonList((spec)));
 
+        // Infinite timeout for Watch API
+        client.getHttpClient().setReadTimeout(0, TimeUnit.SECONDS);
+
         Configuration.setDefaultApiClient(client);
         return client;
     }
@@ -123,7 +131,6 @@ public class KabaneroClient {
 
             KabaneroInstance kabInst = new KabaneroInstance(username, instanceName, date, kabaneroRepositories, clusterName, kabaneroCollections, cliURL);
             LOGGER.log(Level.FINE, "Kabanero Instance: {0}: {1}", new Object[]{ kabInst.getInstanceName(), kabInst});
-
             kabaneroInstances.add(kabInst);
         }
         return kabaneroInstances;
@@ -184,7 +191,7 @@ public class KabaneroClient {
         return collections;
     }
 
-    private static List<KubeKabanero> listKabaneroInstances(ApiClient apiClient, String namespace) throws ApiException {
+    private static List<KubeKabanero> listKabaneroInstances(ApiClient apiClient, String namespace) throws ApiException, IOException {
         CustomObjectsApi customApi = new CustomObjectsApi(apiClient);
         String group = "kabanero.io";
         String version = "v1alpha1";
@@ -192,26 +199,44 @@ public class KabaneroClient {
 
         List<KubeKabanero> instances = new ArrayList<KubeKabanero>();
 
-        Object obj = customApi.listNamespacedCustomObject(group, version, namespace, plural, "true", "", "", 60, false);
-        Map<String, ?> map = (Map<String, ?>) obj;
-        List<Map<String, ?>> items = (List<Map<String, ?>>) map.get("items");
-        for (Map<String, ?> item : items) {
-            Map<String, ?> metadata = (Map<String, ?>) item.get("metadata");
-            String name = (String) metadata.get("name");
-            String creationTime = (String) metadata.get("creationTimestamp");
+        String rs = null;
+        if (resourceVersion > 0)
+            rs = Integer.toString(resourceVersion);
 
-            KubeKabanero instance = new KubeKabanero(name, creationTime);
+        Watch<Object> watch = Watch.createWatch(
+            apiClient, 
+            customApi.listNamespacedCustomObjectCall(group, version, namespace, plural, "true", "", rs, 5, true, null, null), 
+            new TypeToken<Watch.Response<Object>>(){}.getType());
 
-            Map<String, ?> spec = (Map<String, ?>) item.get("spec");
-            if (spec != null) {
-                Map<String, ?> collections = (Map<String, ?>) spec.get("collections");
-                if (collections != null) {
-                    List<Map<String, ?>> repositories = (List<Map<String, ?>>) collections.get("repositories");
-                    instance.setRepositories(repositories);
+        try {
+            for (Watch.Response<Object> obj : watch) {
+                Map<String, ?> item = (Map<String, ?>) obj.object;
+                Map<String, ?> metadata = (Map<String, ?>) item.get("metadata");
+
+                String name = (String) metadata.get("name");
+                String creationTime = (String) metadata.get("creationTimestamp");
+
+                int resourceVersionNew = Integer.parseInt((String) metadata.get("resourceVersion"));
+
+                if (resourceVersionNew > resourceVersion) {
+                    KubeKabanero instance = new KubeKabanero(name, creationTime);
+
+                    Map<String, ?> spec = (Map<String, ?>) item.get("spec");
+                    if (spec != null) {
+                        Map<String, ?> collections = (Map<String, ?>) spec.get("collections");
+                        if (collections != null) {
+                            List<Map<String, ?>> repositories = (List<Map<String, ?>>) collections.get("repositories");
+                            instance.setRepositories(repositories);
+                        }
+                        instances.add(instance);
+                        resourceVersion = resourceVersionNew;
+                    }
                 }
             }
-            instances.add(instance);
+        } finally {
+            watch.close();
         }
+
         return instances;
     }
 
